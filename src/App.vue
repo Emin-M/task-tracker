f649f8194202291435cecb6425179cd7d53fa17d
Emin-M <mustafazadeemin2000@gmail.com>
2023-11-15 11:04:01 +0400

    feat: Tasks component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,34 @@
 <template>
   <div class="container">
-    <Header title="Task Tracker" />
+    <Header title="Task Tracker"/>
+    <Tasks :tasks="tasks"/>
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
+import Tasks from "./components/Tasks.vue";
+
 export default {
   name: "App",
   components: {
-    Header
+    Header,
+    Tasks
+  },
+  data() {
+    return {
+      tasks: []
+    }
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: "Doctor Appointment",
+        day: "March 1st at 2:30pm",
+        reminder: true,
+      }
+    ]
   }
 }
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,28 +1,29 @@
 <template>
   <header>
     <h1>{{ title }}</h1>
-    <Button text="Add Task" color="green" />
+    <Button text="Add Task" color="green"/>
   </header>
 </template>
 
 <script>
 import Button from "./Button.vue";
-  export default {
-    name: "Header",
-    props: {
-      title: String
-    },
-    components: {
-      Button
-    }
+
+export default {
+  name: "Header",
+  props: {
+    title: String
+  },
+  components: {
+    Button
   }
+}
 </script>
 
 <style scoped>
-  header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 20px;
-  }
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
 </style>
