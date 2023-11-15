bf439c106e53b03d540b1c5692883d36cfbad05a
Emin-M <mustafazadeemin2000@gmail.com>
2023-11-15 11:51:25 +0400

    feat: Task delete

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,7 +1,7 @@
 <template>
   <div :class="[task.reminder ? 'reminder' : '', 'task']">
     <h3>{{ task.text }}
-      <i class="fas fa-times"></i>
+      <i @click="onDelete(task.id)" class="fas fa-times"></i>
     </h3>
     <p>{{ task.day }}</p>
   </div>
@@ -16,6 +16,11 @@ export default {
       text: String,
       day: String,
       reminder: Boolean
+    }
+  },
+  methods: {
+    onDelete(id) {
+      this.$emit('delete-task', id);
     }
   }
 }
